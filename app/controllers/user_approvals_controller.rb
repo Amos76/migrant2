7cc8f8d4ca13f7e414b5988f7d6472b4b63f6dbf
Tom Cartwright <tecartwright@gmail.com>
2016-03-02 09:57:32 +0000

    Only admins can see the user approvals page

--- app/controllers/user_approvals_controller.rb
+++ app/controllers/user_approvals_controller.rb
@@ -1,9 +1,14 @@
 class UserApprovalsController < ApplicationController
   def index
     # Good god this is ugly
-    approved_users = User.with_role(:approved).pluck(:id)
-    @unapproved_users = User.where.not(id: approved_users)
-      .paginate(:page => params[:page])
+    if current_user && current_user.has_role?(:admin)
+      approved_users = User.with_role(:approved).pluck(:id)
+      @unapproved_users = User.where.not(id: approved_users)
+        .paginate(:page => params[:page])
+    else
+      flash[:notice] = "You are not permitted on this page"
+      redirect_to root_path
+    end
   end
 
   def create
