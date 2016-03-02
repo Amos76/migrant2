1cb16b565795fdae79f527ed704725a8f44ac916
Tom Cartwright <tecartwright@gmail.com>
2016-03-02 09:33:30 +0000

    Fix admin mailer

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,8 +1,6 @@
 class AdminMailer < ActionMailer::Base
   layout 'mail_layout'
-  def new_sign_up(name, email)
-    @name = name
-    @email = email
+  def new_sign_up(user)
     User.with_role(:admin).each do |admin|
       mail(from: admin.email, subject: "New user signup")
     end
